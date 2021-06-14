3d45b3583682a3e47d10ca1c65bb75dd8f2aaae8
B-slang <entr.brijesh@gmail.com>
2021-06-14 20:36:48 +0530

    day6 copleted

--- DAY6/daysix.js
+++ DAY6/daysix.js
@@ -61,6 +61,24 @@ console.log(arrClone([1, 2, [4, 0]]));
 // [7, 9, 0, -2]
 // []
 
+// Task 3
+const first =  (array, n)=> {
+      if (array == null) 
+      return void 0;
+    if (n == null) 
+      return array[0];
+    if (n < 0)
+      return [];
+    return array.slice(0, n);
+  };
+  console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+
+
+
 
 // 4.Write a simple JavaScript program to join all elements of the following array into a string.
 // Sample array : myColor = ["Red", "Green", "White", "Black"];
